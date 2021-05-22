18d2c9d44daa9e0628432ce41be15eb5f9dbadcf
Jaitesh Pachipulusu <jaiteshp@gmail.com>
2021-05-22 10:43:21 +0530

    Added notebook for running perturbations

---
+++ launchNotebook.bat
@@ -0,0 +1,5 @@
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+cd C:\Users\Jaitesh\Desktop\Courses\TDAA\CS6100-Christofides-ShortCutting-Heurisitcs
+call conda activate tf2.4
+call jupyter notebook
+pause
